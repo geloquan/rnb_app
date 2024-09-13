4139b2d274b32d873ef9c981f92959570250a1be
IKHUpy <72260774+IKHUpy@users.noreply.github.com>
2024-09-14 02:03:27 +0800

    new approach to focus - UNDONE

--- src/entity/mod.rs
+++ src/entity/mod.rs
@@ -216,7 +216,7 @@ impl Entity {
 
         let g_element = Regex::new(r#"<\s*>\s*<g[^>]*>"#).unwrap();
         
-        let focus_style = r#"style="stroke: #000000 !important""#;
+        let focus_style = r#"style="stroke: red !important""#;
         let unfocus_style = r#"style="stroke: none !important; fill: none !important""#;
 
         let mut x: HashMap<String, String> = HashMap::new();
@@ -251,7 +251,9 @@ impl Entity {
                     .split('_') 
                     .collect::<Vec<&str>>();
                     
+                    clog!(format!("data_name_property: {:?}", data_name_property));
                     clog!(format!("data_name_value: {:?}", data_name_value));
+                    clog!(format!("data_name_properties: {:?}", data_name_properties));
 
                     let floor: String = floor.clone().unwrap_or("".to_string());
 
@@ -307,6 +309,7 @@ impl Entity {
                 .into_iter()
                 .filter(|range| unique_ranges.insert((range.start, range.end)))
                 .collect();
+            unique_ranges_vec.sort_by(|a, b| b.start.cmp(&a.start));
 
             let mut to_focus_unique_ranges = HashSet::new();
             let to_focus_unique_ranges_vec: Vec<Range<i32>> = to_focus_ranges
@@ -317,24 +320,69 @@ impl Entity {
             
             
             let mut sorted_elements: Vec<_> = element.clone().into_iter().collect();
-            sorted_elements.sort_by(|a, b| a.0 .1.end.cmp(&b.0 .1.end));
+            sorted_elements.sort_by(|a, b| a.0 .2.end.cmp(&b.0 .2.end));
             clog!(format!("sorted elements: {:?}", sorted_elements));
+            clog!(format!("unique_ranges_vec: {:?}", unique_ranges_vec));
             clog!(format!("to_focus_unique_ranges_vec: {:?}", to_focus_unique_ranges_vec));
             if let Some(ref mut svg_raw_content) = svg_raw_content {
-                while let Some(ele) = sorted_elements.last() {
-                    if to_focus_unique_ranges_vec.iter().any(|range| {
-                        clog!(format!("range: {:?}", range));
-                        clog!(format!("ele.0.1: {:?}", ele.0.1));
-                        if let Some(substring) = svg_raw_content.get(range.start as usize..range.end as usize) {
-                            clog!("Substring: {}", substring);
-                        };
-                        range.contains(&(ele.0.1.end as i32)) ||
-                        range.end == ele.0.1.end as i32
-                    }) {
-                        svg_raw_content.insert_str((ele.0.1.end).try_into().unwrap(), focus_style);
-                    } else {
-                        svg_raw_content.insert_str((ele.0.1.end).try_into().unwrap(), unfocus_style);
+                for unique_ranges_vece in &unique_ranges_vec {
+                    let closing_bracket: Option<usize> = svg_raw_content.chars().skip(unique_ranges_vece.end as usize).position(|c| c == '>').map(|pos| pos + unique_ranges_vece.end as usize);
+                    if let Some(closing_bracket) = closing_bracket {
+                        clog!(format!("closing_bracket0: {:?}", closing_bracket));
+                        if unique_ranges_vece.end as usize <= closing_bracket && closing_bracket <= svg_raw_content.len() {
+                            clog!(format!("closing_bracket1: {:?}", closing_bracket));
+                            let substring = &svg_raw_content[unique_ranges_vece.end as usize..closing_bracket];
+                            clog!(format!("substring: {:?}", substring));
+                            if let Some(pos) = substring.find(focus_style) {
+                                clog!("REPLACE");
+                                let replace_start = unique_ranges_vece.end as usize + pos;
+                                let replace_end = replace_start + focus_style.len();
+
+                                if to_focus_unique_ranges_vec.iter().any(|to_range| {
+                                    to_range.end == unique_ranges_vece.end
+                                }){
+                                    clog!("focus_style replace");
+                                    svg_raw_content.replace_range(replace_start..replace_end, focus_style);
+                                } else {
+                                    clog!("unfocus_style replace");
+                                    svg_raw_content.replace_range(replace_start..replace_end, unfocus_style);
+                                }
+                                
+                                svg_raw_content.replace_range(replace_start..replace_end, unfocus_style);
+                            } else {
+                                clog!("NOT REPLACE");
+                                if to_focus_unique_ranges_vec.iter().any(|to_range| {
+                                    to_range.end == unique_ranges_vece.end
+                                })
+                                {
+                                    clog!("focus_style");
+                                    svg_raw_content.insert_str((unique_ranges_vece.end).try_into().unwrap(), focus_style);
+                                } else {
+                                    clog!("unfocus_style");
+                                    svg_raw_content.insert_str((unique_ranges_vece.end).try_into().unwrap(), unfocus_style);
+                                }
+                            }
+                        }
                     }
+                }
+                while let Some(ele) = sorted_elements.last() {
+                    //if to_focus_unique_ranges_vec.iter().any(|range| {
+                    //    if let Some(substring) = svg_raw_content.get(range.start as usize..range.end as usize) {
+                    //        clog!("Substring: {}", substring);
+                    //        if let Some(pos) = substring.find(focus_style) {
+                    //            let replace_start = range.start as usize + pos;
+                    //            let replace_end = replace_start + focus_style.len();
+                    //            
+                    //            svg_raw_content.replace_range(replace_start..replace_end, unfocus_style);
+                    //        }
+                    //    };
+                    //    range.contains(&(ele.0.2.end as i32)) ||
+                    //    range.end == ele.0.2.end as i32
+                    //}) {
+                    //    svg_raw_content.insert_str((ele.0.2.end).try_into().unwrap(), focus_style);
+                    //} else {
+                    //    svg_raw_content.insert_str((ele.0.2.end).try_into().unwrap(), unfocus_style);
+                    //}
                     sorted_elements.pop();
                 }
                     //if to_focus_unique_ranges_vec.contains(last_element) {
@@ -353,7 +401,9 @@ impl Entity {
 
         if slot.is_none() { return Err("nothing to process") }
         else if slot.unwrap() == self.focus_option.clone().unwrap_or("".to_string()) { return Err("nothing to process") }
-
+        
+        clog!(format!("slot: {:?}", slot));
+        
         let g_tag = Regex::new(r#"<g\b[^>]*>(.*?)<\/g>|<polygon\b[^>]*>(.*?)<\/polygon>|<g\b[^>]*\/>|<polygon\b[^>]*\/>"#).unwrap();
         let shape_tag = Regex::new(r#"<(polygon)\b[^>]*>(.*?)"#).unwrap();
         let data_name_property = Regex::new(r#"data-name="([^"]+)""#).unwrap();
